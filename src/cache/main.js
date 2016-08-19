3ddceea7329b6d8b7ef964b8ae2947ef30d95ae7
teivaz <teivaz@rambler.ru>
2016-08-20 01:06:21 +0200

    Added missing file

---
+++ src/cache/main.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const Fs = require('fs')
+const Templates = require('../templates/main')
+const formats = require('../renderer/main').formats
+const defaultFormat = require('../renderer/main').defaultFormat
+
+module.exports = function(template, user_id, format) {
+	template = Templates(template)
+	if(!(format in formats)) {
+		format = defaultFormat
+	} 
+	var path = __dirname +
+		'/../../cache/' +
+		template.name + '/' +
+		user_id + '.' + format
+
+	if(Fs.existsSync(path)){
+		return {
+			data: Fs.readFileSync(path),
+			type: formats[format]
+		}
+	}
+	else {
+		return null
+	}
+}
